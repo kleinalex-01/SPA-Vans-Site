62785a3cba28b4f9f8fe9dfb40ceec22fa041aef
kleinalex-01 <kleinalex215@gmail.com>
2025-01-13 16:12:54 +0100

    Cleaned up the remaining components
    Changes:.
     - Cleaned up code for better readability
     - VanType, Van, VanPage.jsx

--- src/Components/Van.jsx
+++ src/Components/Van.jsx
@@ -1,59 +1,32 @@
 import {React, useEffect, useState} from "react";
-import vanImg from "./placeholder-img.jpg"
 import { Link } from "react-router-dom";
 
 export default function Van({ vans , setVans}) {
 
     const placeholderArr = [1, 2, 3, 4, 5, 6];
-    const [loadingDelay, setLoadingDelay] = useState(true);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoadingDelay(false);
-        }, 2000);
-        return () => clearTimeout(timer);
-    }, [])
-
-    const vanList = vans.map((van) => (
-        <div id="van-card-container" key={van.id}>
-        <Link to={`/Vans/${van.id}`} className="trans-hover" ><img class="trans-hover van-img" src={van.imageUrl} alt={van.name} /></Link>
-          <div className="van-card-info d-flex justify-content-between">
-            <Link to={`/Vans/${van.id}`} class="text-decoration-none primary-color"><h3>{van.name}</h3></Link>
-            <div className="d-flex flex-column align-items-end fw-bold fs-5 van-price-info">
-              <p>${van.price}</p>
-              <p>/day</p>
-            </div>
-          </div>
-          <button class={`${van.type === "simple" ? "simple-color" : van.type === "luxury" ? "luxury-color" : van.type === "rugged" ? "rugged-color" : ""}
-            btn btn-warning position-relative rent-btn simple-color`}>{van.type.charAt(0).toUpperCase() + van.type.slice(1)}</button>;
-        </div>
-      ));
-
-
 
     return (
         <>
           <div className="container my-5">
             <div className="row">
               {vans.map((van) => (
-                <div className="col-6 mb-4" key={van.id}>
+                <div className="col-6 mb-4 van-card" key={van.id}>
                   <div id="van-card-container">
                     <Link to={`/Vans/${van.id}`}>
-                    <img className="van-img" src={van.imageUrl} alt={van.name} />
+                      {placeholderArr.includes(van.id) && <img src="./placeholder-img.jpg" alt="placeholder" class="van-img-placeholder"/>}
+                      <img className="van-img" src={van.imageUrl} alt={van.name} />
                     </Link>
-                  <div className="van-card-info d-flex justify-content-between">
-                    <Link to={`/Vans/${van.id}`} className="text-decoration-none primary-color">
-                    <h3>{van.name}</h3>
+                  <div className="van-card-info d-flex justify-content-between p-1">
+                    <Link to={`/Vans/${van.id}`} className="text-decoration-none van-card-van-text-decor">
+                      <h3>{van.name}</h3>
                     </Link>
-                  <div className="d-flex flex-column align-items-end fw-bold fs-5 van-price-info">
-                    <p>${van.price}</p>
-                    <p>/day</p>
-                </div>
-                </div>
-                <button className={`${van.type === "simple" ? "simple-color" : van.type === "luxury" ? "luxury-color" : van.type === "rugged" ? "rugged-color" : ""} 
-                                  btn`}>
-                                  {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
-                </button>
+                  <div className="d-flex align-items-center justify-content-end fw-bold fs-5 van-price-info flex-wrap">
+                    <button className={`${van.type === "simple" ? "simple-color" : van.type === "luxury" ? "luxury-color" : van.type === "rugged" ? "rugged-color" : ""} btn fw-bold van-card-type-bg`}>
+                                      {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+                    </button>
+                    <Link to={`/Vans/${van.id}`} class="fw-normal text-decoration-none van-card-van-text-decor">${van.price}/day</Link>
+                  </div>
+                  </div>
                 </div>
               </div>
               ))}
--- src/Components/VanType.jsx
+++ src/Components/VanType.jsx
@@ -29,19 +29,34 @@ export default function VanType( { vans } ) {
                 </div>
             </nav>
 
-        <div>
-            <Link to="/Vans" class="container-fluid p-5 primary-color ">Back to all vans</Link>
-            <div id="van-img-container">
-                <img className="van-img" src={van.imageUrl} alt={van.name} />
+            <div class="container mt-4">
+                <div class="row">
+                    <div class="col-12 mb-3 van-card">
+                        <Link to="/Vans" class="text-decoration-none text-color-dark fs-3 fw-semibold">Back to all vans</Link>
+                    </div>
+                </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div id="van-img-container" class="text-center">
+                        <img class="img-fluid van-img" src={van.imageUrl} alt={van.name} />
+                    </div>
+                </div>
+            <div class="col-md-6">
+                <div id="van-info-container">
+                    <h3>{van.name}</h3>
+                    <p>{van.description}</p>
+                    <p class="fw-bold">${van.price}/day</p>
+                    <button class="btn rent-btn rent-van-big mt-5">
+                        Rent this van
+                    </button>
+                </div>
             </div>
-            <div id="van-info-container">
-                <h3>{van.name}</h3>
-                <p>{van.description}</p>
-                <p>${van.price}/day</p>
             </div>
-            <button class="btn btn-warning position-relative rent-btn rent-van-big"> Rent this van</button>
+            </div>
 
-        </div>
+        <footer class="footer mt-5">
+            <p class="mb-0">&copy; 2022 #VANLIFE</p>
+        </footer>
         </>
     )
 }
